62353dd652b33be117a06d5d3fad1499f1909d21
Ludaman <jeff.lee.choate@gmail.com>
2018-01-28 16:43:48 -0800

    Add Grid based on user input

--- designs.js
+++ designs.js
@@ -4,7 +4,30 @@
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
-
 // Your code goes here!
+	let parentCanvas = $('#pixelCanvas');
+	let columnHTML = '<td></td>';
+	let rowHTML = '<tr></tr>';
 
+	//remove all old cells
+	$('tr').remove();
+
+	//add rows
+	let height = $('#inputHeight').val();
+	for (var row = 0; row < height; row++) {
+		parentCanvas.append(rowHTML);
+	}
+
+	//add column cells
+	let width = $('#inputWidth').val();
+	for(var column = 0; column < width; column++ ){
+			$('tr').append(columnHTML);
+	}
+	console.log($('#inputHeight').val() + '  '+ $('#inputWidth').val());
 }
+
+$('#addGrid').on('click', function(evt){
+	evt.preventDefault();
+	makeGrid();
+	console.log('Finished addGRid call');
+});
